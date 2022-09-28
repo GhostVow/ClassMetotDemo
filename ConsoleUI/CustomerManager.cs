b6e60dc12e238491bc5345ea43e525ab8853afb7
turko <turko@HAKAN>
2022-09-28 13:32:44 +0300

    Add project files.

---
+++ ConsoleUI/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine($"Müşteri eklendi. Müşteri ID: {customer.Id}");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Müşteri silindi.");
+        }
+
+        public void GetList()
+        {
+            Console.WriteLine("Müşteri Listelendi.");
+        }
+
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static CustomerManager _customerManager;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                _customerManager = new CustomerManager();
+
+                int choice;
+                string choiceScreenText = "[1] - Müşteri Ekle \n[2] - Müşteri Sil \n[3] - Müşterileri Listele \n[4] - Exit";
+
+                Console.WriteLine(choiceScreenText);
+                Console.Write("İşlem: ");
+                choice = int.Parse(Console.ReadLine());
+
+                SelectRouting(choice);
+                Console.ReadLine();
+                Console.Clear();
+            }
+            
+
+        }
+
+        static void SelectRouting(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    Add();
+                    break;
+                case 2:
+                    Delete();
+                    break;
+                case 3:
+                    GetList();
+                    break;
+                case 4:
+                    Exit();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void Exit()
+        {
+            Environment.Exit(-1);
+        }
+
+        static void Add()
+        {
+            Customer customer = new Customer();
+
+            customer.Id = CreateId();
+            Console.Write("Müşteri İsim: ");
+            customer.FirstName = Console.ReadLine();
+
+            Console.Write("Müşteri Soyisim: ");
+            customer.LastName = Console.ReadLine();
+
+            Console.Write("Müşteri Email: ");
+            customer.Email = Console.ReadLine();
+
+
+            _customerManager.Add(customer);
+
+
+        }
+
+        static void Delete()
+        {
+            Customer customer = new Customer();
+
+            Console.Write("Müşteri Id: ");
+            customer.Id = int.Parse(Console.ReadLine());
+
+            _customerManager.Delete(customer);
+        }
+
+        static void GetList()
+        {
+
+            _customerManager.GetList();
+        }
+
+        static int CreateId()
+        {
+            int id = 0;
+            return id;
+        }
+
+    }
+}
